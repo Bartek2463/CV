1db85ba9147a9447e86f53b64d597c649388e3f3
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-07-07 09:50:39 +0200

    CV-01_Create_Education_Service_add_Repository_update Entity

--- CV/src/main/java/com/example/cv/controller/EducationController.java
+++ CV/src/main/java/com/example/cv/controller/EducationController.java
@@ -1,13 +1,29 @@
 package com.example.cv.controller;
 
+import com.example.cv.model.Education;
+import com.example.cv.model.Experience;
+import com.example.cv.service.AboutService;
+import com.example.cv.service.EducationService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.catalina.LifecycleState;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@RequestMapping("/education")
 public class EducationController {
-    @GetMapping("/education")
-    String getEducation(){
-        return "education/education";
-    }
+
+    private EducationService educationService;
+
 
 }
