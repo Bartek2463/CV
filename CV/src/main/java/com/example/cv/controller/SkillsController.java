c1ac26cd4e5384c8614bc8def8deae8c5ca4efe2
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-07-11 15:34:23 +0200

    CV-12 Create Controller for Skills and update views,refactoring code navigation Main views

--- CV/src/main/java/com/example/cv/controller/SkillsController.java
+++ CV/src/main/java/com/example/cv/controller/SkillsController.java
@@ -1,13 +1,38 @@
 package com.example.cv.controller;
 
+import com.example.cv.model.About;
+import com.example.cv.model.Skill;
+import com.example.cv.service.AboutService;
+import com.example.cv.service.SkillService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
 
 @Controller
+@AllArgsConstructor
+@RequestMapping("skills")
 public class SkillsController {
 
-    @GetMapping("/skills")
-    public String getSkills(){
+    private SkillService skillService;
+    private AboutService aboutService;
+
+    @GetMapping
+    public String getSkill(Model model){
+        List<About> allAbouts = aboutService.getAllAbouts();
+        model.addAttribute("about",allAbouts);
         return "skills/skills";
     }
+    @PostMapping
+    public RedirectView postAddSkill(Skill skill){
+        skillService.addSkill(skill);
+        return new RedirectView("/skills");
+    }
+
+
 }
