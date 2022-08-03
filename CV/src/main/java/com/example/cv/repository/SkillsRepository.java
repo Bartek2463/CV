13e9a8d0586f8f1e1a7c811c8694e6a06264409f
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-08-03 15:51:58 +0200

    CV - 15 Create Validation About finish and, add Valid adnotation in Controllers

--- CV/src/main/java/com/example/cv/repository/SkillsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.cv.repository;
-
-import com.example.cv.model.Skill;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SkillsRepository extends JpaRepository<Skill,Long> {
-}
